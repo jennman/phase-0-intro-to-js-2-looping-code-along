e02fb7bbd76d6f9eb1619aa07001401daff47734
Jennifer Man <jenniferman702@gmail.com>
2022-09-22 21:04:55 -0400

    commit tree df96f9b1914355535c501d0e133808b782254665

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+//   }
+//   const gifts = ["teddy bear", "drone", "doll"];
+
+//   function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//       console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//       debugger;
+//     }
+  
+//     return gifts;
+//   }
+  
+//   wrapGifts(gifts);
+
+  const name = (['Guadalupe', 'Ollie','Aki']);
+
+  function writeCards(name,event) {
+    let message = [];
+    for (let i = 0; i < name.length; i++){
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    debugger;
+}
+return message;
+
+}
+function countDown(){
+    let down = 10;
+    while (down > -1){
+        console.log(down--);
+    }
+    
+}
